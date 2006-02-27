2df4cece30133f99836adc6cdaec31849413009e
lbarbisan <lbarbisan@c2726117-310c-0410-ad51-ad2f86534f6c>
2006-02-27 05:40:15 +0000

    Mise à jour script

--- Applet/install-applet.sh
+++ Applet/install-applet.sh
@@ -4,6 +4,6 @@ echo =========================L O A D  A P P L E T=========================
 packages =  sys.argv[1].split(\".\")
 print \"/\".join(packages)" $4`/javacard/applet.ijc -package_id A0000000$3 -applet_id A0000000$3$1
 echo =====================I N S T A L L  A P P L E T=======================
-./run.sh -install 500 -package_id A0000000$3 -applet_id A0000000$3$1
+./run.sh -install 800 -package_id A0000000$3 -applet_id A0000000$3$1
 echo ========================== T E R M I N E=======================
 
